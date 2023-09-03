40c6168ead29235cb2c74a6adf63f78196493917
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-09-03 01:06:07 -0300

    Pipes numbers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 // Angular
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 
 // Routing
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +10,13 @@ import { SharedModule } from './shared/shared.module';
 
 // Components
 import { AppComponent } from './app.component';
+
+// Languages
+import localeEsAR from '@angular/common/locales/es-AR';
+import localeFrCA from '@angular/common/locales/fr-CA';
+
+registerLocaleData(localeEsAR);
+registerLocaleData(localeFrCA);
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +26,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-AR' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
